e14dab3eea1b76061b0d572f30ffd4f1cd957adc
rishubhjain <rishubhjain47@gmail.com>
2017-08-22 12:14:28 +0530

    Functionality to update alert dashboards

---
+++ tendrl/monitoring_integration/flows/update_dashboard/__init__.py
@@ -0,0 +1,82 @@
+import json
+import copy
+
+
+from tendrl.monitoring_integration.flows.update_dashboard.alert_utils import get_alert_dashboard
+from tendrl.monitoring_integration.flows.update_dashboard import alert_utils
+from tendrl.monitoring_integration.grafana import dashboard
+from tendrl.commons import flows
+
+
+class UpdateDashboard(flows.BaseFlow):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        super(UpdateDashboard, self).run()
+        resource_name = str(self.parameters.get("resource_name")).lower()
+        resource_type = str(self.parameters.get("resource_type")).lower()
+        operation = str(self.parameters.get("operation")).lower()
+        cluster_id = self.parameters.get("TendrlContext.integration_id")
+        if operation.lower() == "add":
+            self._add_panels(cluster_id, resource_type, resource_name)
+        elif operation.lower() == "delete":
+            self._delete_panel(cluster_id,resource_type, resource_name = None)
+    
+
+    def _add_panel(self, cluster_id, resource_type, resource_name = None):
+
+        if resource_type == "volume":
+
+            alert_dashboard = alert_utils.get_alert_dashboard("volumes")
+            alert_row = alert_utils.fetch_row(alert_dashboard)
+            alert_utils.add_volume_panel(alert_row, cluster_id, resource_name)
+            dash_json = alert_utils.create_updated_dashboard(alert_dashboard, alert_row)
+            resp = dashboard._post_dashboard(json.dumps(dash_json))
+            return resp
+
+        elif resource_type == "host":
+
+            alert_dashboard = alert_utils.get_alert_dashboard("hosts")
+            alert_row = alert_utils.fetch_row(alert_dashboard)
+            alert_utils.add_node_panel(alert_row, cluster_id, resource_name)
+            dash_json = alert_utils.create_updated_dashboard(alert_dashboard, alert_row)
+            resp = dashboard._post_dashboard(json.dumps(dash_json))
+            return resp
+
+        elif resource_type == "brick":
+
+             alert_dashboard = alert_utils.get_alert_dashboard("bricks")
+             alert_row = alert_utils.fetch_row(alert_dashboard)
+             alert_utils.add_brick_panel(alert_row, cluster_id, resource_name)
+             dash_json = alert_utils.create_updated_dashboard(alert_dashboard, alert_row)
+             resp = dashboard._post_dashboard(json.dumps(dash_json))
+             return resp
+
+        #Tendrl At a Glance Alerts
+        elif resource_type == "cluster":
+
+             alert_dashboard = alert_utils.get_alert_dashboard("at-a-glance")
+             alert_row = alert_utils.fetch_row(alert_dashboard)
+             alert_utils.add_cluster_panel(alert_row, cluster_id)
+             dash_json = alert_utils.create_updated_dashboard(alert_dashboard, alert_row)
+             resp = dashboard._post_dashboard(json.dumps(dash_json))
+             return resp
+
+    def _delete_panel(self, cluster_id,resource_type, resource_name = None):
+
+        if resource_name is None:
+            resource_type = "at-a-glance"
+        alert_dashboard = alert_utils.get_alert_dashboard(resource_type)
+        alert_utils.remove_row(alert_dashboard, cluster_id, resource_name)
+        resp = dashboard._post_dashboard(json.dumps(alert_dashboard))
+        response = []
+        response.append(copy.deepcopy(resp))
+        if resource_name is None:
+            dashboard_names = ["volumes", "hosts", "bricks"]
+            for dash_name in dashboard_names:
+                alert_dashboard = alert_utils.remove_cluster_rows(cluster_id, dash_name)
+                resp = dashboard._post_dashboard(json.dumps(alert_dashboard))
+                response.append(copy.deepcopy(resp))
+        return response
---
+++ tendrl/monitoring_integration/flows/update_dashboard/alert_utils.py
@@ -0,0 +1,132 @@
+import copy
+
+
+from tendrl.monitoring_integration.grafana import dashboard
+
+
+def get_alert_dashboard(dashboard_name):
+    slug = "alerts-tendrl-gluster-" + str(dashboard_name)
+    dashboard_json = dashboard.get_dashboard(slug)
+    return dashboard_json
+
+
+def fetch_row(dashboard_json):
+
+    rows = dashboard_json["dashboard"]["rows"]
+    if len(rows) > 1:
+        for count in xrange(1,len(rows)):
+            if rows[0]["panels"][0]["title"].split("-",1)[0] == rows[count]["panels"][0]["title"].split("-",1)[0]:
+                alert_row = copy.deepcopy(dashboard_json["dashboard"]["rows"][-count:])
+                break
+    else:
+        alert_row = [copy.deepcopy(dashboard_json["dashboard"]["rows"][-1])]
+    return alert_row
+
+
+def add_volume_panel(alert_rows, cluster_id, volume_name):
+
+    for alert_row in alert_rows:
+        panel_count = alert_row["panels"][-1]["id"] + 1
+        for panel in alert_row["panels"]:
+            targets = panel["targets"]
+            for target in targets:
+                panel_target = ("tendrl" + target["target"].split("tendrl")[1].split(")")[0]).split(".")
+                old_cluster_id = panel_target[panel_target.index("clusters") + 1]
+                target["target"] = target["target"].replace(old_cluster_id, str(cluster_id))
+                old_volume_name = panel_target[panel_target.index("volumes") + 1]
+                target["target"] = target["target"].replace(old_volume_name, str(volume_name))
+            panel["id"] = panel_count
+            panel_count = panel_count + 1
+            panel["title"] = panel["title"].split("-",1)[0] + "-" + str(volume_name)
+
+
+def add_cluster_panel(alert_rows, cluster_id):
+
+    for alert_row in alert_rows:
+        panel_count = alert_row["panels"][-1]["id"] + 1
+        for panel in alert_row["panels"]:
+            targets = panel["targets"]
+            for target in targets:
+                panel_target = ("tendrl" + target["target"].split("tendrl")[1].split(")")[0]).split(".")
+                old_cluster_id = panel_target[panel_target.index("clusters") + 1]
+                target["target"] = target["target"].replace(old_cluster_id, str(cluster_id))
+            panel["id"] = panel_count
+            panel_count = panel_count + 1
+            panel["title"] = panel["title"].split("-",1)[0] + "-" + str(volume_name)
+
+
+def add_brick_panel(alert_rows, cluster_id, brick_name):
+
+    for alert_row in alert_rows:
+        panel_count = alert_row["panels"][-1]["id"] + 1
+        for panel in alert_row["panels"]:
+            targets  = panel["targets"]
+            for target in targets:
+                panel_target = ("tendrl" + target["target"].split("tendrl")[1].split(")")[0]).split(".")
+                old_cluster_id = panel_target[panel_target.index("clusters") + 1]
+                target["target"] = target["target"].replace(old_cluster_id, str(cluster_id))
+                old_brick_name = panel_target[panel_target.index("bricks") + 1]
+                target["target"] = target["target"].replace(old_brick_name, str(brick_name))
+            panel["id"] = panel_count
+            panel_count = panel_count + 1
+            panel["title"] = panel["title"].split("-",1)[0] + "-" + str(brick_name)
+
+def add_node_panel(alert_rows, cluster_id, node_name):
+
+    for alert_row in alert_rows:
+        panel_count = alert_row["panels"][-1]["id"] + 1
+        for panel in alert_row["panels"]:
+            targets  = panel["targets"]
+            for target in targets:
+                panel_target = ("tendrl" + target["target"].split("tendrl")[1].split(")")[0]).split(".")
+                old_cluster_id = panel_target[panel_target.index("clusters") + 1]
+                target["target"] = target["target"].replace(old_cluster_id, str(cluster_id))
+                old_node_name = panel_target[panel_target.index("nodes") + 1]
+                target["target"] = target["target"].replace(old_node_name, str(node_name))
+            panel["id"] = panel_count
+            panel_count = panel_count + 1
+            panel["title"] = panel["title"].split("-",1)[0] + "-" + str(node_name)
+
+
+def remove_row(alert_dashboard, cluster_id, resource_name):
+
+    rows  = alert_dashboard["dashboard"]["rows"]
+    new_rows = []
+    flag = True
+    for row in rows:
+        for target in row["panels"][0]["targets"]:
+            # TODO(Rishubh) the case where the cluster id and resource name are on different targets
+            if resource_name is not None:
+                if str(cluster_id) in target["target"] and str(resource_name) in target["target"]:
+                    flag = False
+                    break
+            else:
+                if str(cluster_id) in target["target"]:
+                    flag = False
+                    break
+        if flag:
+            new_rows.append(row)
+        flag = True
+    alert_dashboard["dashboard"]["rows"] = new_rows
+
+
+def remove_cluster_rows(cluster_id, dashboard_name):
+    
+    alert_dashboard = get_alert_dashboard(dashboard_name)
+    new_rows = []
+    flag = True
+    rows  = alert_dashboard["dashboard"]["rows"]
+    for row in rows:
+        for target in row["panels"][0]["targets"]:
+            if str(cluster_id) in target["target"]:
+                flag = False
+                break
+        if flag:
+            new_rows.append(row)
+        flag = True
+    alert_dashboard["dashboard"]["rows"] = new_rows
+    return alert_dashboard
+
+def create_updated_dashboard(dashboard_json, alert_rows):
+    dashboard_json["dashboard"]["rows"] = dashboard_json["dashboard"]["rows"] + alert_rows
+    return dashboard_json
