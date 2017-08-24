5e5a0ae166097a2e2863f64ba946becda22c3b3b
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-08-24 15:54:37 +0530

    Fetch cluster metrics only if Tendrl managed cluster
    tendrl-bug-id: Tendrl/monitoring-integration#40

--- tendrl/monitoring_integration/sync/__init__.py
+++ tendrl/monitoring_integration/sync/__init__.py
@@ -15,22 +15,26 @@ class MonitoringIntegrationSdsSyncThread(sds_sync.StateSyncThread):
         super(MonitoringIntegrationSdsSyncThread, self).__init__()
         self._complete = gevent.event.Event()
         self.plugin_obj = GraphitePlugin()
+        self.sync_interval = None
 
     def _run(self):
-        interval = etcd_utils.read("_NS/gluster/config/data/sync_interval")
-
-        try:
-            interval = float(interval.value)
-        except ValueError as ex:
-            logger.log("error", NS.get("publisher_id", None),
-                       {'message': str(ex)})
-            raise ex
-
-        aggregate_gluster_objects  = NS.monitoring.definitions.get_parsed_defs()["namespace.monitoring"]["graphite_data"]
-
         while not self._complete.is_set():
+            if self.sync_interval is None:
+                try:
+                    interval = etcd_utils.read("_NS/gluster/config/data/sync_interval")
+                    try:
+                        self.sync_interval = float(interval.value)
+                    except ValueError as ex:
+                        logger.log("error", NS.get("publisher_id", None),
+                               {'message': "Unable to parse tendrl-gluster-integration config 'sync_interval' (value: %s)" % interval.value})
+                        raise ex
+                except etcd.EtcdKeyNotFound as ex:
+                    continue
+
+            aggregate_gluster_objects  = NS.monitoring.definitions.get_parsed_defs()["namespace.monitoring"]["graphite_data"]
+
             try:
-                gevent.sleep(interval)
+                gevent.sleep(self.sync_interval)
                 cluster_details = self.plugin_obj.get_central_store_data(aggregate_gluster_objects)
                 metrics = graphite_utils.create_metrics(aggregate_gluster_objects, cluster_details)
                 for metric in metrics:
