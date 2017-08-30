4ed7fa6bdede5dabdc84dffe417b360c7d874cb4
GowthamShanmugam <gshanmug@redhat.com>
2017-08-30 13:14:46 +0530

    Modified webhook receiver as class and added exception handling
    Signed-off-by: GowthamShanmugam <gshanmug@redhat.com>

--- tendrl/monitoring_integration/grafana/webhook_receiver.py
+++ tendrl/monitoring_integration/grafana/webhook_receiver.py
@@ -1,13 +1,9 @@
 import gevent
-import gevent.event
 import gevent.greenlet
 import json
 
 from gevent.pywsgi import WSGIServer
-from gevent.socket import error as socket_error
-from gevent.socket import timeout as socket_timeout
 from gevent.server import StreamServer
-from io import BlockingIOError
 from tendrl.commons.event import Event
 from tendrl.commons.message import ExceptionMessage
 from tendrl.monitoring_integration.alert.handlers import AlertHandlerManager
@@ -24,38 +20,42 @@ class WebhookReceiver(gevent.greenlet.Greenlet):
         
     def application(self, env, start_response):
         try:
-            if env['PATH_INFO'] == '/grafana_callback':
+            if env['PATH_INFO'] != '/grafana_callback':
+                start_response(
+                    '404 Not Found',
+                    [('Content-Type', 'text/html')]
+                )
+                response = [b'<h1>Alert Not Found</h1>']
+            else:
+                data = env['wsgi.input'].read()
+                data = json.loads(data)
+                self.alert_handler.handle_alert(
+                    data["ruleId"]
+                )
                 start_response(
                     '200 OK',
                     [('Content-Type', 'text/html')]
                 )
-                data = env['wsgi.input'].read()
-                data = json.loads(data)
-                self.alert_handler.handle_alert(data["ruleId"])
-                return [b"<b>Recieved</b>"]
-        except (socket_error, socket_timeout) as ex:
+                response = [b'<h1>Alert Received</h1>']
+        except (IOError, AssertionError) as ex:
             Event(
                 ExceptionMessage(
                     priority="error",
                     publisher=NS.publisher_id,
                     payload={
-                        "message": "webhook receiver socket error",
+                        "message": "Unable to read alert from socket",
                         "exception": ex
                     }
                 )
             )
-        start_response(
-            '404 Not Found',
-            [('Content-Type', 'text/html')]
-        )
-        return [b'<h1>Not Found</h1>']
+            response = [b'<h1>Error in reading alert from socket</h1>']
+
+        return response
 
     def _run(self):
         try:
             self.server.serve_forever()
         except (TypeError,
-                BlockingIOError,
-                socket_error,
                 ValueError) as ex:
             Event(
                 ExceptionMessage(
