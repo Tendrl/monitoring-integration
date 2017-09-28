52137b9d82b0647cc2b32cea009ecbed4f02306a
rishubhjain <rishubhjain47@gmail.com>
2017-09-28 16:07:08 +0530

    Removing host details in graphite

--- tendrl/monitoring_integration/flows/update_graphite/__init__.py
+++ tendrl/monitoring_integration/flows/update_graphite/__init__.py
@@ -5,6 +5,8 @@ import datetime
 
 from tendrl.commons import flows
 from tendrl.commons.utils import log_utils as logger
+from tendrl.commons.utils import etcd_utils
+from tendrl.monitoring_integration.grafana import create_dashboards
 
 
 WHISPER_PATH = "/var/lib/carbon/whisper/tendrl/"
@@ -18,10 +20,11 @@ class UpdateGraphite(flows.BaseFlow):
             self.parameters.get("Trigger.resource_name")).lower()
         resource_type = str(
             self.parameters.get("Trigger.resource_type")).lower()
-        self.update_graphite(cluster_id, resource_name, resource_type)
+        self.update_graphite(cluster_id, resource_name,
+                             resource_type.lower())
 
-    def update_graphite(cluster_id, resource_name, resource_type):
-        if resource_type = "volume":
+    def update_graphite(self, cluster_id, resource_name, resource_type):
+        if resource_type == "volume":
             resource_path = os.path.join(WHISPER_PATH, "clusters",
                                          str(cluster_id),
                                          "volumes", resource_name)
@@ -29,46 +32,141 @@ class UpdateGraphite(flows.BaseFlow):
                                         str(cluster_id),
                                         "archive", "volumes")
             if not os.path.exists(archive_path):
-                os.system("mkdir " + str(archive_path))
+                os.system("mkdir -p " + str(archive_path))
             resource_folder_name = str(resource_name) + "_" + \
                 str(datetime.datetime.now().isoformat())
             archive_path = os.path.join(archive_path, resource_folder_name)
-            os.system("mkdir " + str(archive_path))
             if os.path.exists(resource_path):
-                os.system("mv " + str(resource_path) + " " + str(archive_path) + "/.")
-            if os.path.exists(os.path.join(str(archive_path), resource_name)):
+                os.system("mv " + str(resource_path) + " " +
+                          str(archive_path))
+            if os.path.exists(os.path.join(str(archive_path),
+                                           resource_name)):
                 logger.log("info", NS.get("publisher_id", None),
                            {'message': "Volume " + str(resource_name) + 
                                        "deleted from graphite"})
             else:
                  logger.log("error", NS.get("publisher_id", None),
-                           {'message': "Volume " + str(resource_name) + 
+                           {'message': "Volume " + str(resource_name) +
                                        "deletion from graphite failed"})
-        if resource_type = "brick":
-            host_name = resource_name.split("|", 1)[1].split(":", 1)[0].replace(".", "_")
-            brick_name = resource_name.split("|", 1)[1].split(":", 1)[1].replace("/", '\|')
+        if resource_type == "brick":
+            host_name = resource_name.split("|",
+                                            1)[1].split(":",
+                                                        1)[0].replace(".",
+                                                                      "_")
+            brick_name = resource_name.split("|",
+                                             1)[1].split(":",
+                                                         1)[1].replace("/",
+                                                                       '\|')
             vol_name = resource_name.split("|", 1)[0]
             archive_path = os.path.join(WHISPER_PATH, "clusters",
                                         str(cluster_id),
                                         "archive", "bricks")
             if not os.path.exists(archive_path):
-                os.system("mkdir " + str(archive_path))
+                os.system("mkdir -p " + str(archive_path))
             resource_folder_name = str(brick_name) + "_" + \
                 str(datetime.datetime.now().isoformat())
             archive_path = os.path.join(archive_path, resource_folder_name)
-            os.system("mkdir " + str(archive_path))
             resource_path = os.path.join(WHISPER_PATH, "clusters",
                                          str(cluster_id), "nodes",
                                          str(host_name), "bricks",
                                          str(brick_name))
             if os.path.exists(resource_path):
-                os.system("mv " + str(resource_path) + " " + str(archive_path) + "/.")
+                os.system("mv " + str(resource_path) + " " +
+                          str(archive_path))
             if os.path.exists(os.path.join(str(archive_path), brick_name)):
                 logger.log("info", NS.get("publisher_id", None),
-                           {'message': "Brick " + str(resource_name) + 
+                          {'message': "Brick " + str(resource_name) + 
                                        "deleted from graphite"})
             else:
                  logger.log("error", NS.get("publisher_id", None),
                            {'message': "Brick " + str(resource_name) + 
                                        "deletion from graphite failed"})
-
+            archive_path = os.path.join(archive_path,
+                                        "volumes", vol_name)
+            resource_path = os.path.join(WHISPER_PATH, "clusters",
+                                         str(cluster_id), "volumes",
+                                         str(vol_name), "nodes",
+                                         str(host_name), "bricks",
+                                         str(brick_name))
+            if os.path.exists(resource_path):
+                flag = os.system("mv " + str(resource_path) + " " +
+                                 str(archive_path))
+                if flag is 0:
+                    logger.log("info", NS.get("publisher_id", None),
+                              {'message': "Brick " + str(resource_name) + 
+                                       "deleted from graphite"})
+                else:
+                    logger.log("error", NS.get("publisher_id", None),
+                              {'message': "Brick " + str(resource_name) + 
+                                       "deletion from graphite failed"})
+        if resource_type == "host":
+            host_name = resource_name.split(":", 1)[0].replace(".", "_")
+            volume_affected_list = []
+            volume_key = os.path.join("clusters", str(cluster_id), "volumes")
+            volume_list = create_dashboards.get_resource_keys("", volume_key)
+            for volume_id in volume_list:
+                flag = False
+                single_volume_key = os.path.join(volume_key, volume_id,
+                                                 "Bricks")
+                subvolume_list = create_dashboards.get_resource_keys("", single_volume_key)
+                for subvolume in subvolume_list:
+                    subvolume_brick_key = os.path.join(single_volume_key,
+                                                       subvolume)
+                    brick_list = create_dashboards.get_resource_keys("", subvolume_brick_key)
+                    if flag:
+                        flag = False
+                        continue
+                    for brick in brick_list:
+                        subvolume_hostname = brick.split(":",
+                                                         1)[0].replace(".",
+                                                                       "_")
+                        if host_name == subvolume_hostname:
+                            volume_key = os.path.join(volume_key, volume_id,
+                                                      "name")
+                            try:
+                                volume_name = etcd_utils.read(volume_key).value
+                                volume_affected_list.append(volume_name)
+                            except etcd.EtcdKeyNotFound:
+                                pass
+                            flag = True
+                            continue
+            archive_path = os.path.join(WHISPER_PATH, "clusters",
+                                        str(cluster_id),
+                                        "archive", "nodes")
+            if not os.path.exists(archive_path):
+                os.system("mkdir -p " + str(archive_path))
+            resource_folder_name = str(host_name) + "_" + \
+                str(datetime.datetime.now().isoformat())
+            archive_path = os.path.join(archive_path, resource_folder_name)
+            resource_path = os.path.join(WHISPER_PATH, "clusters",
+                                         str(cluster_id), "nodes",
+                                         str(host_name))
+            if os.path.exists(resource_path):
+                os.system("mv " + str(resource_path) + " " +
+                          str(archive_path))
+            if os.path.exists(os.path.join(str(archive_path), brick_name)):
+                logger.log("info", NS.get("publisher_id", None),
+                           {'message': "Host " + str(resource_name) + 
+                                       "deleted from graphite"})
+            else:
+                 logger.log("error", NS.get("publisher_id", None),
+                            {'message': "Host " + str(resource_name) + 
+                                       "deletion from graphite failed"})
+            for vol_name in volume_affected_list:
+                archive_path = os.path.join(archive_path,
+                                            "volumes", vol_name)
+                resource_path = os.path.join(WHISPER_PATH, "clusters",
+                                             str(cluster_id), "volumes",
+                                             str(vol_name), "nodes",
+                                             str(host_name))
+                if os.path.exists(resource_path):
+                    flag = os.system("mv " + str(resource_path) + " " +
+                                     str(archive_path))
+                    if flag is 0:
+                        logger.log("info", NS.get("publisher_id", None),
+                                   {'message': "Host " + str(resource_name) +
+                                       "deleted from graphite"})
+                    else:
+                        logger.log("error", NS.get("publisher_id", None),
+                                   {'message': "Host " + str(resource_name) +
+                                       "deletion from graphite failed"})
