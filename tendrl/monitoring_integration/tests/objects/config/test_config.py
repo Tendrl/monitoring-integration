739f359274ffb20f8ca345aa5d8d841b6298aa57
GowthamShanmugasundaram <gshanmug@redhat.com>
2019-02-20 17:37:46 +0000

    Upstream only: pep8 fix
    bugzilla: 1679060
    tendrl-bug-id: Tendrl/monitoring-integration#591
    
    Signed-off-by: GowthamShanmugasundaram <gshanmug@redhat.com>

--- tendrl/monitoring_integration/tests/objects/config/test_config.py
+++ tendrl/monitoring_integration/tests/objects/config/test_config.py
@@ -8,5 +8,5 @@ from tendrl.monitoring_integration.objects import config
 def test_config(load_config):
     load_config.return_value = "test_monitoring"
     obj = config.Config()
-    if obj.data is not "test_monitoring":
+    if obj.data != "test_monitoring":
         raise AssertionError()
--- tendrl/monitoring_integration/tests/objects/trigger/test_trigger.py
+++ tendrl/monitoring_integration/tests/objects/trigger/test_trigger.py
@@ -3,7 +3,7 @@ from tendrl.monitoring_integration.objects import trigger
 
 def test_config():
     obj = trigger.Trigger("update", "volume", "v1")
-    if obj.resource_name is not "v1" and \
-        obj.resource_type is not "volume" and \
-            obj.action is not "update":
+    if obj.resource_name != "v1" and \
+        obj.resource_type != "volume" and \
+            obj.action != "update":
         raise AssertionError()
